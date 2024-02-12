2f4ad4d667281df0ac900fc47805c7833e8af1e9
Dmitriy Podorozhnykh <veseliy.koteika@yandex.ru>
2024-02-12 09:42:27 +0300

    Настроена сборка webpack для разработки.

---
+++ src/scripts/index.js
@@ -0,0 +1,33 @@
+import '../pages/index.css';
+import {initialCards} from './cards.js';
+
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+// @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
+
+// @todo: Функция создания карточки
+function addCard(link, name, delCard){
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = link;
+    cardImage.alt = name;
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => delCard(cardElement));
+    return cardElement;
+}
+
+// @todo: Функция удаления карточки
+function delCard(cardItem){
+  cardItem.remove();
+}
+
+// @todo: Вывести карточки на страницу
+function showCards(data){
+  data.forEach(element => {
+    cardList.append(addCard(element.link, element.name, delCard));
+  });
+}
+
+showCards(initialCards);
