47426c25131b9e4b1290b972662d116a720ad176
Dmitriy Podorozhnykh <veseliy.koteika@yandex.ru>
2024-02-12 13:35:31 +0300

    Добавлен функционал открытия и закрытия попапов. Редактирование профиля.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,60 @@
 import '../pages/index.css';
 import {initialCards} from './cards.js';
 
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const viewAvatar = document.querySelector('.profile__image');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const popupViewAvatar = document.querySelector('.popup_type_image');
+const popupEditProfile = document.querySelector('.popup_type_edit');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const popupInputName = popupEditProfile.querySelector('.popup__input_type_name');
+const popupInputDescription = popupEditProfile.querySelector('.popup__input_type_description');
+
+
+function editProfile(){
+  if(profileTitle.textContent !== '' || profileDescription.textContent !== ''){
+    popupInputName.value = profileTitle.textContent;
+    popupInputDescription.value = profileDescription.textContent;
+  }
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popupInputName.value;
+  profileDescription.textContent = popupInputDescription.value;
+  this.classList.remove('popup_is-opened');
+  this.removeEventListener('submit', handleFormSubmit);
+}
+
+function openModal(button, modal){
+  button.addEventListener('click', () => {
+    modal.classList.add('popup_is-opened');
+    bindCloseModal(modal);
+    if(modal.classList.contains('popup_type_edit'))
+      editProfile();
+      modal.addEventListener('submit', handleFormSubmit);
+  });
+}
+
+function bindCloseModal(modal){
+
+  const button = modal.querySelector('.popup__close');
+
+  function closeModal(evt){
+    if(evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup') || evt.key === 'Escape'){
+      modal.classList.remove('popup_is-opened');
+      button.removeEventListener('click', closeModal);
+      document.removeEventListener('click', closeModal);
+      document.removeEventListener('keydown', closeModal);
+    }
+  }
+  button.addEventListener('click', closeModal);
+  document.addEventListener('click', closeModal);
+  document.addEventListener('keydown', closeModal);
+}
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
@@ -29,5 +83,9 @@ function showCards(data){
     cardList.append(addCard(element.link, element.name, delCard));
   });
 }
+
+openModal(addButton, popupNewCard);
+openModal(editButton, popupEditProfile);
+openModal(viewAvatar, popupViewAvatar);
 
 showCards(initialCards);
